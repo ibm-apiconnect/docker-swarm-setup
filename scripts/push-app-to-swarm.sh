a0f90dca91e78485b7baef49a736310fb247f94a
Kevin Delisle <delisle@ca.ibm.com>
2017-03-12 14:40:49 -0400

    Add copyright headers to scripts

--- scripts/push-app-to-swarm.sh
+++ scripts/push-app-to-swarm.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 
+# Licensed Materials - Property of IBM
+# 5725-L30, 5725-Z22
+#
+# (C) Copyright IBM Corporation 2017
+#
+# All Rights Reserved.
+# US Government Users Restricted Rights - Use, duplication or disclosure
+# restricted by GSA ADP Schedule Contract with IBM Corp.
+# Setup host to use registry certificate
+
 # Load config
 if [ ! -z "$SWARM_SETUP_CFG" ] && [ -f "$SWARM_SETUP_CFG" ]; then
   source "$SWARM_SETUP_CFG"
--- setup.sh
+++ setup.sh
@@ -1,4 +1,14 @@
 #!/bin/bash
+
+# Licensed Materials - Property of IBM
+# 5725-L30, 5725-Z22
+#
+# (C) Copyright IBM Corporation 2017
+#
+# All Rights Reserved.
+# US Government Users Restricted Rights - Use, duplication or disclosure
+# restricted by GSA ADP Schedule Contract with IBM Corp.
+
 # The master setup script
 
 # Load config
